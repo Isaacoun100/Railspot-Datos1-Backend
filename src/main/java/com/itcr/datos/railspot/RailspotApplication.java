23720da5748992f260ffee3c9c9379a716601591
Isaac Herrera Monge <51173904+Isaacoun100@users.noreply.github.com>
2020-08-09 20:23:25 -0600

    [Project init] This is the first commit for the Railspot project

---
+++ src/main/java/com/itcr/datos/railspot/RailspotApplication.java
@@ -0,0 +1,13 @@
+package com.itcr.datos.railspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailspotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itcr/datos/railspot/RailspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcr.datos.railspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
