5cc0076cda36eee90da1689c71aecc2c92ce505b
Isaac Herrera Monge <51173904+Isaacoun100@users.noreply.github.com>
2020-08-11 20:35:11 -0600

    [User tree init] The tree inits

--- src/main/java/com/itcr/datos/railspot/RailspotApplication.java
+++ src/main/java/com/itcr/datos/railspot/RailspotApplication.java
@@ -1,5 +1,9 @@
 package com.itcr.datos.railspot;
 
+import com.itcr.datos.railspot.dataStructures.SinglyList;
+import com.itcr.datos.railspot.management.UserTree;
+import com.itcr.datos.railspot.objects.Ticket;
+import com.itcr.datos.railspot.objects.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RailspotApplication {
 
 	public static void main(String[] args) {
+
+		UserTree.initUserTree();
 		SpringApplication.run(RailspotApplication.class, args);
 	}
 
