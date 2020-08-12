819db354e5dc90857a205e1e53e2e7e1ed64c136
Isaac Herrera Monge <51173904+Isaacoun100@users.noreply.github.com>
2020-08-12 11:57:31 -0600

    [Users init in the main]

--- src/main/java/com/itcr/datos/railspot/RailspotApplication.java
+++ src/main/java/com/itcr/datos/railspot/RailspotApplication.java
@@ -1,9 +1,6 @@
 package com.itcr.datos.railspot;
 
-import com.itcr.datos.railspot.dataStructures.SinglyList;
 import com.itcr.datos.railspot.management.UserTree;
-import com.itcr.datos.railspot.objects.Ticket;
-import com.itcr.datos.railspot.objects.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
