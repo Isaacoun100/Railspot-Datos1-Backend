da7f04b81ac066cd020c3e56248e352456b1eab2
Isaac Herrera Monge <51173904+Isaacoun100@users.noreply.github.com>
2020-08-11 20:36:58 -0600

    [Prints the tree] You can now access the tree with the link

--- src/main/java/com/itcr/datos/railspot/host/WebHost.java
+++ src/main/java/com/itcr/datos/railspot/host/WebHost.java
@@ -1,5 +1,6 @@
 package com.itcr.datos.railspot.host;
 
+import com.itcr.datos.railspot.management.UserTree;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,8 @@ public class WebHost {
 
     @GetMapping("/")
     public String login(){
-        return "Welcome to RailSpot";
+        UserTree.saveUser();
+        return UserTree.userTree.toString();
     }
 
 }
