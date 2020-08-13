453ea6280cf4ede89781af0189a25ca5725c36e9
NJohnsonCR <60128330+NJohnsonCR@users.noreply.github.com>
2020-08-13 08:12:22 -0600

    User method

--- src/main/java/com/itcr/datos/railspot/host/WebHost.java
+++ src/main/java/com/itcr/datos/railspot/host/WebHost.java
@@ -1,7 +1,11 @@
 package com.itcr.datos.railspot.host;
 
+import com.itcr.datos.railspot.dataStructures.NodeAVL;
 import com.itcr.datos.railspot.management.UserTree;
+import com.itcr.datos.railspot.objects.User;
+import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +15,15 @@ public class WebHost {
     public String login(){
         UserTree.saveUser();
         return UserTree.userTree.toString();
+    }
+
+    @GetMapping("/getUser/{userKey}")
+    public JSONObject getUser(@PathVariable int userKey){
+        try {
+            NodeAVL<User> user = UserTree.searchUser(userKey);
+            return UserTree.userToJSON(user);
+        }
+        catch (NullPointerException e){return UserTree.userToJSON(null);}
     }
 
 }
